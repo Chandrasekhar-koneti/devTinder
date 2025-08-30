d9f7fa940a58c7e79874da50a470bd9fd71fa769
chandrasekharkoneti <chandrasekhar@finsire.com>
2025-08-30 21:17:22 +0530

    first push into devTinder

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/dashboard", (req, res) => {
+  res.send("server running on dashboard");
+});
+
+app.get("/", (req, res) => {
+  res.send("starting server");
+});
+
+app.use((req, res) => {
+  res.status(404).send("Route not found");
+});
+
+app.listen(2222, () => {
+  console.log("server is running on port 2222");
+});

b090975f255f048d8ef73809aba611e7a04f0f2c
chandrasekharkoneti <chandrasekhar@finsire.com>
2025-10-15 13:34:07 +0530

    created user routes

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -3,10 +3,19 @@ const mongoose = require("mongoose");
 
 const connectionModelSchema = new mongoose.Schema(
   {
-    fromUserId: { type: mongoose.Schema.Types.ObjectId },
-    toUserId: { type: mongoose.Schema.Types.ObjectId },
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     status: {
       type: String,
+      required: true,
       enum: {
         values: ["interested", "ignored", "accepted", "rejected"],
         message: "{VALUE} is incorrect status type",

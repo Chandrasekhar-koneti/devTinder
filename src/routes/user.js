713697096cdd44e8482dbc113461faa548e59c43
chandrasekharkoneti <chandrasekhar@finsire.com>
2025-10-15 17:04:34 +0530

    user feed api created

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-
+const User = require("../models/user");
 const userRouter = express.Router();
 
 const USER_SAFE_DATA = "firstName lastName photoUrl about age gender skills";
@@ -39,6 +39,32 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       return row.fromUserId;
     });
     res.send({ data });
+  } catch (err) {
+    res.send({ error: err.message });
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const usersList = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser?._id }, { fromUserId: loggedInUser?._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromList = new Set();
+    usersList.forEach((req) => {
+      hideUsersFromList.add(req.fromUserId);
+      hideUsersFromList.add(req.toUserId);
+    });
+
+    const usersToShow = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromList) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+    res.send({ usersToShow });
   } catch (err) {
     res.send({ error: err.message });
   }

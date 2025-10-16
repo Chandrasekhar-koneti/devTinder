23af37261ebdd208aad0d5388cb052810f1229f2
chandrasekharkoneti <chandrasekhar@finsire.com>
2025-10-16 18:20:36 +0530

    cors fixed

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const connectDB = require("./config/dataBase");
 const app = express();
 const cookieParser = require("cookie-parser");
-app.use(express.json());
-app.use(cookieParser());
+const cors = require("cors");
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
